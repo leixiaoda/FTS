060201e999778873853564059d547894e5d66317
leixiaoda <leixiaoda@gmail.com>
2018-04-19 21:04:31 +0800

    create project

--- FTS/ViewController.swift
+++ FTS/ViewController.swift
@@ -7,14 +7,98 @@
 //
 
 import UIKit
+import SQLite
+//import SQLite3
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.test()
     }
 
+    func test() {
+        // 声明变量
+        var db: Connection!
+//        let normalTable = Table("Files")
+        let table = VirtualTable("Files")
+        let id = Expression<String>("id")
+        let text = Expression<String>("text")
+        var snippet = Expression<String>("")
+
+        // 初始化
+        let sqlFilePath = NSHomeDirectory() + "/Documents" + "/file.sqlite3"
+        var dbConnect: Connection?
+        do {
+            dbConnect = try Connection(sqlFilePath)
+        } catch {
+            debugPrint(error)
+        }
+        db = dbConnect
+
+        // 建表
+        //        do {
+        //            let create = normalTable.create { t in
+        //                t.column(id, primaryKey: true)
+        //                t.column(text)
+        //            }
+        //            try db.run(create)
+        //        } catch {
+        //            print(error)
+        //        }
+        do {
+            let create = table.create(.FTS4(id, text))
+            try db.run(create)
+        } catch {
+            print(error)
+        }
+
+        // 插入数据
+        do {
+            let insert = table.insert(id <- "1", text <- self.article1())
+            try db.run(insert)
+        } catch {
+            print(error)
+        }
+
+        do {
+            let insert = table.insert(id <- "2", text <- "world")
+            try db.run(insert)
+        } catch {
+            print(error)
+        }
+
+        do {
+            let insert = table.insert(id <- "3", text <- "world break")
+            try db.run(insert)
+        } catch {
+            print(error)
+        }
+
+        snippet = self.snippetWrapper(column: text, tableName: "Files")
+
+        // 查询
+        let matchQuery: QueryType = table.select(snippet, id, text).match("all")
+        let results = try? db?.prepare(matchQuery)
+
+        if let concreteResults = results {
+            let _ = concreteResults?.compactMap({
+                print("\($0[id]), \($0[snippet]), \($0[text])")
+            })
+        }
+    }
+
+    func article1() -> String {
+        guard let path = Bundle.main.path(forResource: "article1", ofType: "txt") else { return ""}
+        let content = try? String(contentsOfFile: path, encoding: String.Encoding.utf8)
+
+        return content ?? ""
+    }
+
+    func snippetWrapper(column: Expression<String>, tableName: String) -> Expression<String> {
+        return Expression("snippet(\(tableName))", column.bindings)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '9.0'
+
+target 'FTS' do
+  pod 'SQLite.swift'
+end
